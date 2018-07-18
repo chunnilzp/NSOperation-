10b43694d83a3e3a6342057ab79f4fa4b66359ce
lizp <lizp@lhzdtech.com>
2018-07-18 15:07:41 +0800

    Initial Commit

---
+++ StudyNSOperation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StudyNSOperation
+//
+//  Created by 李泽平 on 2018/7/18.
+//  Copyright © 2018年 李泽平. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ StudyNSOperationTests/StudyNSOperationTests.m
@@ -0,0 +1,39 @@
+//
+//  StudyNSOperationTests.m
+//  StudyNSOperationTests
+//
+//  Created by 李泽平 on 2018/7/18.
+//  Copyright © 2018年 李泽平. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface StudyNSOperationTests : XCTestCase
+
+@end
+
+@implementation StudyNSOperationTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
